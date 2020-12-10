cc3b14de2d87991a7c989d2f86e7f56cd42b866e
hoangtuyet0202 <65934949+hoangtuyet0202@users.noreply.github.com>
2020-12-10 11:36:30 +0700

    Add files via upload

---
+++ src/com/model/TaiKhoanModel.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+/**
+ *
+ * @author VITQUAY
+ */
+public class TaiKhoanModel {
+    private String username, password;
+    private int id;
+
+    public TaiKhoanModel() {
+    }
+
+    public TaiKhoanModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
