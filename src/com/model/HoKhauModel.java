68ebe6d856ed14644e7e61adacbc5805a59f0000
RV12309 <65643163+RV12309@users.noreply.github.com>
2020-12-12 13:22:19 +0700

    Update HoKhauModel.java

--- src/com/model/HoKhauModel.java
+++ src/com/model/HoKhauModel.java
@@ -10,5 +10,41 @@ package com.model;
  * @author VITQUAY
  */
 public class HoKhauModel {
+    private int maHK;
+    private String tenHK;
+    private int soNhanKhau;
+    private String diaChi;
+
+    public int getMaHK() {
+        return maHK;
+    }
+
+    public void setMaHK(int maHK) {
+        this.maHK = maHK;
+    }
+
+    public String getTenHK() {
+        return tenHK;
+    }
+
+    public void setTenHK(String tenHK) {
+        this.tenHK = tenHK;
+    }
+
+    public int getSoNhanKhau() {
+        return soNhanKhau;
+    }
+
+    public void setSoNhanKhau(int soNhanKhau) {
+        this.soNhanKhau = soNhanKhau;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
     
 }
