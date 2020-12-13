2e1cf354c9243bff5715588cf0cd5718d5ec4978
hoangviet661999 <72456767+hoangviet661999@users.noreply.github.com>
2020-12-13 18:04:56 +0700

    add dong gop DAO

--- src/com/dao/impl/DongGopDAO.java
+++ src/com/dao/impl/DongGopDAO.java
@@ -1,38 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.dao.impl;
+package com.laptrinhjavaweb.dao.impl;
 
-import com.dao.IDongGopDAO;
-import com.model.DongGopModel;
 import java.util.List;
 
-/**
- *
- * @author VITQUAY
- */
-public class DongGopDAO extends AbstractDAO<DongGopModel> implements IDongGopDAO{
+import com.laptrinhjavaweb.dao.ICategoryDAO;
+import com.laptrinhjavaweb.mapper.CategoryMapper;
+import com.laptrinhjavaweb.model.CategoryModel;
 
-    @Override
-    public List<DongGopModel> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public class DongGopDAO extends AbstractDAO<DongGopModel> implements IDongGopDAO {
 
-    @Override
-    public List<DongGopModel> findByTheLoai(int maTL) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public List<DongGopModel> findAll() {
+		String sql = "SELECT * FROM category";
+		return query(sql, new CategoryMapper());
+	}
+	
+	@Override
+	public List<DongGopModel> findByTheLoai(int maTL){
+		String sql = "SELECT maTL FROM category";
+		return query(sql, new CategoryMapper());
+	}
 
-    @Override
-    public void insert(DongGopModel newDG) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	@Override
+        public void insert(DongGopModel newDG) {
+        String sql = "INSERT INTO (maDongGop, maHoKhau, soTienDG, maTheLoaiDG, ngayDG) VALUES (?,?,?,?,?)";
+        insert(sql,newDG.getMaDongGop(), newDG.getMaHoKhau(), newDG.getSoTienDG(), newDG.getMaTheLoaiDG(), newDG.getNgayDG);        
+        }
+	
+        @Override
+        public void update(DongGopModel newDG) {
+    	String sql = "UPDATE DongGop SET maDongGop = ?, maHoKhau  = ?, soTienDG = ?, maTheLoaiDG = ?,  ngayDG = ? WHERE maDongGop = ?";
+    	update(sql, updateDG.getMaDongGop(), updateDG.getMaHoKhau(), updateDG.getSoTienDG(), updateDG.getMaTheLoaiDG(), updateDG.getNgayDG());
     }
-
-    @Override
-    public void update(DongGopModel updateDG) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+  
 }
