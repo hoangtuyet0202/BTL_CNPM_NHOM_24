09be646382aabb29f95a5a076a3905d0257592ae
hoangviet661999 <72456767+hoangviet661999@users.noreply.github.com>
2020-12-13 18:16:00 +0700

    add DongGop model

--- src/com/model/DongGopModel.java
+++ src/com/model/DongGopModel.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.model;
 
-/**
- *
- * @author VITQUAY
- */
-public class DongGopModel {
-    
+import java.sql.Timestamp;
+
+public class DongGopModel extends AbstractModel<DongGopModel>{
+	private int maDongGop;
+	private int maHoKhau;
+	private int soTienDG;
+	private int maTheLoaiDG;
+	private Timestamp ngayDG;
+
+	public DongGopModel() {
+	}
+	
+	public int getMaDongGop() {
+		return this.maDongGop;
+	}
+	public void setMaDongGop(int maDongGop) {
+		this.maDongGop = maDongGop;
+	}
+	public int getMaHoKhau() {
+		return this.maHoKhau;
+	}
+	public void setMaHoKhau(int maHoKhau) {
+		this.maHoKhau = maHoKhau;
+	}
+	public int getSoTienDG() {
+		return this.soTienDG;
+	}
+	public void setSoTienDG(int soTienDG) {
+		this.soTienDG = soTienDG;
+	}
+	public int getMaTheLoaiDG() {
+		return this.maTheLoaiDG;
+	}
+	public void setMaTheLoaiDG(int maTheLoaiDG) {
+		this.maTheLoaiDG = maTheLoaiDG;
+	}
+	public Timestamp getNgayDG() {
+		return this.ngayDG;
+	}
+	public void setNgayDG(Timestamp ngayDG) {
+		this.ngayDG = ngayDG;
+	}	
 }
