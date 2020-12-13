40c40b5f24bdc33f967a2e5f55e425a0f2c9cf53
ukkk2349 <72452940+ukkk2349@users.noreply.github.com>
2020-12-13 13:59:22 +0700

    Update TheLoaiDongGopModel.java
    add theloaiDG

--- src/com/model/TheLoaiDongGopModel.java
+++ src/com/model/TheLoaiDongGopModel.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.model;
 
-/**
- *
- * @author VITQUAY
- */
+import java.util.Date;
+
 public class TheLoaiDongGopModel {
+
+    private int maTheLoaiDG;
+    private String tenTheLoaiDG;
+    private Date ngayBDThu;
+
+    public TheLoaiDongGopModel() {
+    }
+
+    public TheLoaiDongGopModel(int maTheLoaiDG, String tenTheLoaiDG, Date ngayBDThu) {
+        this.maTheLoaiDG = maTheLoaiDG;
+        this.tenTheLoaiDG = tenTheLoaiDG;
+        this.ngayBDThu = ngayBDThu;
+    }
+    
+    public int getMaTheLoaiDG() {
+        return maTheLoaiDG;
+    }
+
+    public void setMaTheLoaiDG(int maTheLoaiDG) {
+        this.maTheLoaiDG = maTheLoaiDG;
+    }
+
+    public String getTenTheLoaiDG() {
+        return tenTheLoaiDG;
+    }
+
+    public void setTenTheLoaiDG(String tenTheLoaiDG) {
+        this.tenTheLoaiDG = tenTheLoaiDG;
+    }
+
+    public Date getNgayBDThu() {
+        return ngayBDThu;
+    }
+
+    public void setNgayBDThu(Date ngayBDThu) {
+        this.ngayBDThu = ngayBDThu;
+    }
+
+    
+    @Override
+    public String toString() {
+        return this.tenTheLoaiDG; //To change body of generated methods, choose Tools | Templates.
+    }
+
     
 }
