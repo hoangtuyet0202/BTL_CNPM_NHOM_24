e1214c637dd2e8e733d515293ca592b8560fa56d
hoangtuyet0202 <65934949+hoangtuyet0202@users.noreply.github.com>
2020-12-13 21:28:59 +0700

    Update DongGopDAO.java

--- src/com/dao/impl/DongGopDAO.java
+++ src/com/dao/impl/DongGopDAO.java
@@ -1,35 +1,70 @@
-package com.laptrinhjavaweb.dao.impl;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao.impl;
 
+import com.dao.IDongGopDAO;
+import com.mapper.DongGopMapper;
+import com.model.DongGopModel;
 import java.util.List;
 
-import com.laptrinhjavaweb.dao.ICategoryDAO;
-import com.laptrinhjavaweb.mapper.CategoryMapper;
-import com.laptrinhjavaweb.model.CategoryModel;
+/**
+ *
+ * @author VITQUAY
+ */
+public class DongGopDAO extends AbstractDAO<DongGopModel> implements IDongGopDAO{
 
-public class DongGopDAO extends AbstractDAO<DongGopModel> implements IDongGopDAO {
-
-	@Override
-	public List<DongGopModel> findAll() {
-		String sql = "SELECT * FROM category";
-		return query(sql, new CategoryMapper());
-	}
-	
-	@Override
-	public List<DongGopModel> findByTheLoai(int maTL){
-		String sql = "SELECT maTL FROM category";
-		return query(sql, new CategoryMapper());
-	}
-
-	@Override
-        public void insert(DongGopModel newDG) {
-        String sql = "INSERT INTO (maDongGop, maHoKhau, soTienDG, maTheLoaiDG, ngayDG) VALUES (?,?,?,?,?)";
-        insert(sql,newDG.getMaDongGop(), newDG.getMaHoKhau(), newDG.getSoTienDG(), newDG.getMaTheLoaiDG(), newDG.getNgayDG);        
-        }
-	
-        @Override
-        public void update(DongGopModel newDG) {
-    	String sql = "UPDATE DongGop SET maDongGop = ?, maHoKhau  = ?, soTienDG = ?, maTheLoaiDG = ?,  ngayDG = ? WHERE maDongGop = ?";
-    	update(sql, updateDG.getMaDongGop(), updateDG.getMaHoKhau(), updateDG.getSoTienDG(), updateDG.getMaTheLoaiDG(), updateDG.getNgayDG());
+    String sql;
+    @Override
+    public List<DongGopModel> findAll() {
+        sql = "SELECT * FROM donggop";
+        return query(sql, new DongGopMapper());
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-  
+
+    @Override
+    public DongGopModel findByDG(int maDG) {
+        sql = "SELECT * FROM donggop WHERE MaDongGop = ?";
+        return (DongGopModel) query(sql, new DongGopMapper(), maDG).get(0);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insert(DongGopModel newDG) {
+        sql = "INSERT INTO donggop(MaDongGop, MaHoKhau, SoTienDG, MaTheLoaiDG, NgayDongGop, GhiChu) VALUES (?,?,?,?,?,?)";
+        insert(sql, newDG.getMaDongGop(), newDG.getMaHoKhau(), newDG.getSoTienDG(), newDG.getMaTheLoaiDG(), newDG.getNgayDG(), newDG.getGhiChu());
+
+//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(DongGopModel updateDG) {
+        sql = "UPDATE donggop SET MaHoKhau = ?, SoTienDG = ?, MaTheLoaiDG= ?, NgayDongGop= ?, GhiChu= ? WHERE MaDongGop = ?";
+        update(sql, updateDG.getMaHoKhau(), updateDG.getSoTienDG(), updateDG.getMaTheLoaiDG(), updateDG.getNgayDG(), updateDG.getGhiChu(), updateDG.getMaDongGop());
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<DongGopModel> findByTheLoai(int maTL) {
+        sql = "SELECT * FROM donggop WHERE MaTheLoaiDG = ?";
+        return query(sql, new DongGopMapper(), maTL);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int maDG) {
+        sql = "DELETE FROM donggop WHERE MaDongGop = ?";
+        update(sql, maDG);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<DongGopModel> findByMaHK(int maHK) {
+        sql = "SELECT * FROM donggop WHERE MaHoKhau = ?";
+        return query(sql, new DongGopMapper(), maHK);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
