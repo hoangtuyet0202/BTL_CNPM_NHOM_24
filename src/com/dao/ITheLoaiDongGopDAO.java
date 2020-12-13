a3806b38b2d02e7bc0751581ef19487dbbd17e36
ukkk2349 <72452940+ukkk2349@users.noreply.github.com>
2020-12-13 14:01:33 +0700

    add theloaidonggop
    add theloaidonggop

--- src/com/dao/ITheLoaiDongGopDAO.java
+++ src/com/dao/ITheLoaiDongGopDAO.java
@@ -6,15 +6,21 @@
 package com.dao;
 
 import com.model.TheLoaiDongGopModel;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
-/**
- *
- * @author VITQUAY
- */
-public interface ITheLoaiDongGopDAO extends IGenericDAO<TheLoaiDongGopModel>{
+public interface ITheLoaiDongGopDAO extends IGenericDAO<TheLoaiDongGopModel> {
+
     List<TheLoaiDongGopModel> findAll();
+
     void insert(TheLoaiDongGopModel newTL);
-    void update (TheLoaiDongGopModel updateTL);
+
+    void update(TheLoaiDongGopModel updateTL);
+
     void delete(int maTL);
+    
+    TheLoaiDongGopModel findByMa(int maTL);
+    
+    Vector findTL(ArrayList<TheLoaiDongGopModel> listTL);
 }
