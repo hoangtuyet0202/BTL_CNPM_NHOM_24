b60ca00b74d7763f3637544360d982e09175abda
ukkk2349 <72452940+ukkk2349@users.noreply.github.com>
2020-12-13 14:21:33 +0700

    add the loai dong gop

--- src/com/mapper/TheLoaiDongGopMapper.java
+++ src/com/mapper/TheLoaiDongGopMapper.java
@@ -1,22 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mapper;
 
+import com.model.TheLoaiDongGopModel;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.model.TheLoaiDongGopModel;
-
-public class TheLoaiDongGopMapper implements RowMapper<TheLoaiDongGopModel>{
+public class TheLoaiDongGopMapper implements RowMapper{
 
     @Override
     public TheLoaiDongGopModel mapRow(ResultSet rs) {
         try {
-			TheLoaiDongGopModel tl = new TheLoaiDongGopModel();
-			tl.setMaTheLoaiDG(rs.getInt("MaTheLoaiDG"));
-			tl.setTenLoaiDG(rs.getString("TenLoaiDG"));
-			return tl;
-		} catch (SQLException e) {
-			return null;
-		}
+            TheLoaiDongGopModel tl = new TheLoaiDongGopModel();
+            tl.setMaTheLoaiDG(rs.getInt("MaTheLoaiDG"));
+            tl.setTenTheLoaiDG(rs.getString("TenLoaiDG"));
+            tl.setNgayBDThu(rs.getDate("NgayBatDauThu"));
+            return tl;
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } catch (SQLException ex) {
+            Logger.getLogger(TaiKhoanMapper.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
     }
     
 }
