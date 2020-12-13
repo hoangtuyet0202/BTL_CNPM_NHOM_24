543e01611625714912249dc6f8079aaaf95195e0
hoangtuyet0202 <65934949+hoangtuyet0202@users.noreply.github.com>
2020-12-13 21:18:59 +0700

    Update DongGopMapper.java

--- src/com/mapper/DongGopMapper.java
+++ src/com/mapper/DongGopMapper.java
@@ -1,23 +1,36 @@
-public com.mapper;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mapper;
 
+import com.model.DongGopModel;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class DongGopMapper implements RowMapper<DongGopModel> {
+/**
+ *
+ * @author VITQUAY
+ */
+public class DongGopMapper implements RowMapper {
 
-	@Override
-	public DongGopModel mapRow(ResultSet resultSet) {
-		try {
-			DongGopModel DG = new DongGopModel();
-			DG.setMaDongGop(resultSet.getInt("madonggop"));
-			DG.setMaHoKhau(resultSet.getInt("mahokhau"));
-			DG.setSoTienDG(resultSet.getInt("sotiendg"));
-			DG.setMaTheLoaiDG(resultSet.getInt("matheloaidg"));
-			DG.setNgayDongGop(resultSet.getTimestamp("ngaydonggop"));
-			
-			}
-			return DG;
-		} catch (SQLException e) {
-			return null;
-		}	
-	}
+    @Override
+    public DongGopModel mapRow(ResultSet resultSet) {
+        try {
+            DongGopModel DG = new DongGopModel();
+            DG.setMaDongGop(resultSet.getInt("MaDongGop"));
+            DG.setMaHoKhau(resultSet.getInt("MaHoKhau"));
+            DG.setSoTienDG(resultSet.getInt("SoTienDG"));
+            DG.setMaTheLoaiDG(resultSet.getInt("MaTheLoaiDG"));
+            DG.setNgayDG(resultSet.getDate("NgayDongGop"));
+            DG.setGhiChu(resultSet.getString("GhiChu"));
+            return DG;
+        } catch (SQLException e) {
+            return null;
+
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+    }
 }
