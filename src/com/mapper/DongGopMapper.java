88640ef5d52354ecd66e976c85ab5d39ebe5b747
hoangviet661999 <72456767+hoangviet661999@users.noreply.github.com>
2020-12-13 18:17:41 +0700

    add DongGop mapper

--- src/com/mapper/DongGopMapper.java
+++ src/com/mapper/DongGopMapper.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mapper;
+public com.mapper;
 
 import java.sql.ResultSet;
 
-/**
- *
- * @author VITQUAY
- */
-public class DongGopMapper implements RowMapper{
+public class DongGopMapper implements RowMapper<DongGopModel> {
 
-    @Override
-    public Object mapRow(ResultSet rs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+	@Override
+	public DongGopModel mapRow(ResultSet resultSet) {
+		try {
+			DongGopModel DG = new DongGopModel();
+			DG.setMaDongGop(resultSet.getInt("madonggop"));
+			DG.setMaHoKhau(resultSet.getInt("mahokhau"));
+			DG.setSoTienDG(resultSet.getInt("sotiendg"));
+			DG.setMaTheLoaiDG(resultSet.getInt("matheloaidg"));
+			DG.setNgayDongGop(resultSet.getTimestamp("ngaydonggop"));
+			
+			}
+			return DG;
+		} catch (SQLException e) {
+			return null;
+		}	
+	}
 }
