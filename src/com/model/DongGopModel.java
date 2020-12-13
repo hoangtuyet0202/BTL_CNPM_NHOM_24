378572fcb4588533872385bf4e7c6c41449b5f79
hoangtuyet0202 <65934949+hoangtuyet0202@users.noreply.github.com>
2020-12-13 21:17:02 +0700

    Update DongGopModel.java

--- src/com/model/DongGopModel.java
+++ src/com/model/DongGopModel.java
@@ -1,45 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.model;
 
-import java.sql.Timestamp;
+import java.util.Date;
 
-public class DongGopModel extends AbstractModel<DongGopModel>{
-	private int maDongGop;
-	private int maHoKhau;
-	private int soTienDG;
-	private int maTheLoaiDG;
-	private Timestamp ngayDG;
+/**
+ *
+ * @author VITQUAY
+ */
+public class DongGopModel {
 
-	public DongGopModel() {
-	}
-	
-	public int getMaDongGop() {
-		return this.maDongGop;
-	}
-	public void setMaDongGop(int maDongGop) {
-		this.maDongGop = maDongGop;
-	}
-	public int getMaHoKhau() {
-		return this.maHoKhau;
-	}
-	public void setMaHoKhau(int maHoKhau) {
-		this.maHoKhau = maHoKhau;
-	}
-	public int getSoTienDG() {
-		return this.soTienDG;
-	}
-	public void setSoTienDG(int soTienDG) {
-		this.soTienDG = soTienDG;
-	}
-	public int getMaTheLoaiDG() {
-		return this.maTheLoaiDG;
-	}
-	public void setMaTheLoaiDG(int maTheLoaiDG) {
-		this.maTheLoaiDG = maTheLoaiDG;
-	}
-	public Timestamp getNgayDG() {
-		return this.ngayDG;
-	}
-	public void setNgayDG(Timestamp ngayDG) {
-		this.ngayDG = ngayDG;
-	}	
+    private int maDongGop;
+    private int maHoKhau;
+    private int soTienDG;
+    private int maTheLoaiDG;
+    private Date ngayDG;
+    private String ghiChu;
+
+    public DongGopModel() {
+    }
+
+    public DongGopModel(int maDongGop, int maHoKhau, int soTienDG, int maTheLoaiDG, Date ngayDG, String ghiChu) {
+        this.maDongGop = maDongGop;
+        this.maHoKhau = maHoKhau;
+        this.soTienDG = soTienDG;
+        this.maTheLoaiDG = maTheLoaiDG;
+        this.ngayDG = ngayDG;
+        this.ghiChu = ghiChu;
+    }
+
+    public int getMaDongGop() {
+        return this.maDongGop;
+    }
+
+    public void setMaDongGop(int maDongGop) {
+        this.maDongGop = maDongGop;
+    }
+
+    public int getMaHoKhau() {
+        return this.maHoKhau;
+    }
+
+    public void setMaHoKhau(int maHoKhau) {
+        this.maHoKhau = maHoKhau;
+    }
+
+    public int getSoTienDG() {
+        return this.soTienDG;
+    }
+
+    public void setSoTienDG(int soTienDG) {
+        this.soTienDG = soTienDG;
+    }
+
+    public int getMaTheLoaiDG() {
+        return this.maTheLoaiDG;
+    }
+
+    public void setMaTheLoaiDG(int maTheLoaiDG) {
+        this.maTheLoaiDG = maTheLoaiDG;
+    }
+
+    public Date getNgayDG() {
+        return this.ngayDG;
+    }
+
+    public void setNgayDG(Date ngayDG) {
+        this.ngayDG = ngayDG;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
 }
